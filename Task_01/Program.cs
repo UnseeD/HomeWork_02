78a5203f58e53e940c8127996404dc074b2b91f3
DV <unseedevil@gmail.com>
2022-12-21 22:08:50 +0300

    HomeWork

---
+++ Task_01/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите трехзначное число: ");
+int A = int.Parse(Console.ReadLine());
+if (A >= 100 && A < 1000)
+    {
+        int X = (A / 10) % 10;
+        Console.WriteLine($"Искомая цифра вашего числа {X}");
+    }
+    else
+    {
+        System.Console.WriteLine("Вы ввели не трехзначное число");
+    }
